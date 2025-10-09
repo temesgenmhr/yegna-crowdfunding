a31fc1b271b2283860df068d48c9294540e5723e
Temesgen Mihiretu <tmsmhr@gmail.com>
2025-10-09 23:02:53 +0300

    bug fix

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,6 @@ export const config = {
   31337: {  // Hardhat local chain ID
     url: "http://127.0.0.1:8545",
     chainId: 31337,
-    counterAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",  // From deployment
     CrowdFundingAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",  // From deployment
   },
 };
